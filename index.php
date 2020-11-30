82f206e30e95eb8001c1dd46d0a8b2b2e8cac6ab
Michelle Blanchette <michelle@purpleturtlecreative.com>
2020-11-30 07:39:42 -0500

    ENH: content none

--- index.php
+++ index.php
@@ -19,31 +19,31 @@ get_header();
 
 	<main id="primary" <?php post_class( 'site-main template-index' ); ?>>
 
-		<?php if ( have_posts() ) : ?>
+		<header class="page-header wave-trim-bottom">
+			<div class="content-width">
 
-			<header class="page-header wave-trim-bottom">
-				<div class="content-width">
+				<?php
+				$before_page_title = '<h1 class="page-title">';
+				$after_page_title = '</h1>';
 
-					<?php
-					$before_page_title = '<h1 class="page-title">';
-					$after_page_title = '</h1>';
+				if ( is_home() ) {
+					echo wp_kses_post( $before_page_title . '<span>' . single_post_title( '', false ) . '</span>' . $after_page_title );
+				} elseif ( is_search() ) {
+					echo wp_kses_post( $before_page_title . 'Search Results for:<br /><span>' . get_search_query() . '</span>' . $after_page_title );
+				} else {
+					the_archive_title( $before_page_title, $after_page_title );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				}
+				?>
 
-					if ( is_home() ) {
-						echo wp_kses_post( $before_page_title . '<span>' . single_post_title( '', false ) . '</span>' . $after_page_title );
-					} elseif ( is_search() ) {
-						echo wp_kses_post( $before_page_title . 'Search Results for:<br /><span>' . get_search_query() . '</span>' . $after_page_title );
-					} else {
-						the_archive_title( $before_page_title, $after_page_title );
-						the_archive_description( '<div class="archive-description">', '</div>' );
-					}
-					?>
-
-					<div class="all-categories">
-						<?php all_categories(); ?>
-					</div>
-
+				<div class="all-categories">
+					<?php all_categories(); ?>
 				</div>
-			</header><!-- .page-header -->
+
+			</div><!-- .content-width -->
+		</header><!-- .page-header -->
+
+		<?php if ( have_posts() ) : ?>
 
 			<div class="posts-loop content-width">
 				<?php
@@ -57,13 +57,21 @@ get_header();
 				?>
 			</div>
 
-		<?php
-		else :
+		<?php else : ?>
 
-			get_template_part( 'template-parts/content', 'none' );
+			<div class="no-results not-found content-width">
 
-		endif;
-		?>
+			<?php if ( is_search() ) : ?>
+				<p>No posts were found to include your search terms. Try again with some different keywords!</p>
+			<?php else : ?>
+				<p>No posts were found. Try using the search form below or the category buttons above!</p>
+			<?php endif; ?>
+
+			<?php get_search_form(); ?>
+
+			</div><!-- .page-content -->
+
+		<?php endif; ?>
 
 	</main><!-- #main -->
 
