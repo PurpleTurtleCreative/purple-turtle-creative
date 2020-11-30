80a6263867c398e11440626a72c63fc9e7f9d6a5
Michelle Blanchette <michelle@purpleturtlecreative.com>
2020-11-30 08:22:28 -0500

    ENH: Added asset getters

--- functions/tags-assets.php
+++ functions/tags-assets.php
@@ -18,6 +18,18 @@ namespace PTC_Theme;
  * located. Default '' to use the theme's /assets/images/ folder.
  */
 function svg( string $asset_image_filename, string $directory = '' ) {
+	echo get_svg( $asset_image_filename, $directory );//phpcs:ignore
+}
+
+/**
+ * Gets an SVG asset from a file.
+ *
+ * @param string $asset_image_filename The SVG filename.
+ *
+ * @param string $directory Optional. The full path where the SVG file is
+ * located. Default '' to use the theme's /assets/images/ folder.
+ */
+function get_svg( string $asset_image_filename, string $directory = '' ) {
 
 	if ( '' === $directory ) {
 		$directory = get_template_directory() . '/assets/images/';
@@ -38,8 +50,18 @@ function svg( string $asset_image_filename, string $directory = '' ) {
 	}
 
 	// SUCCESS!
-	echo wp_kses_post( $svg );
+	return wp_kses_post( $svg );
 
+}
+
+/**
+ * Outputs an SVG asset's URI.
+ *
+ * @param string $asset_image_filename The SVG filename in the theme's
+ * /assets/images/ folder.
+ */
+function svg_uri( string $asset_image_filename ) {
+	echo get_svg_uri( $asset_image_filename );//phpcs:ignore
 }
 
 /**
@@ -55,16 +77,6 @@ function get_svg_uri( string $asset_image_filename ) {
 }
 
 /**
- * Outputs an SVG asset's URI.
- *
- * @param string $asset_image_filename The SVG filename in the theme's
- * /assets/images/ folder.
- */
-function svg_uri( string $asset_image_filename ) {
-	echo get_svg_uri( $asset_image_filename );
-}
-
-/**
  * Outputs a FontAwesome icon SVG.
  *
  * @param string $icon_name The name of the icon.
@@ -73,6 +85,18 @@ function svg_uri( string $asset_image_filename ) {
  * icon such as 'solid', 'regular', or 'brands'. Default 'solid'.
  */
 function fa( string $icon_name, string $family_dir = 'solid' ) {
+	echo get_fa( $icon_name, $family_dir );//phpcs:ignore
+}
+
+/**
+ * Gets a FontAwesome icon SVG.
+ *
+ * @param string $icon_name The name of the icon.
+ *
+ * @param string $family_dir Optional. The FontAwesome family to retrieve the
+ * icon such as 'solid', 'regular', or 'brands'. Default 'solid'.
+ */
+function get_fa( string $icon_name, string $family_dir = 'solid' ) {
 
 	$full_family_dir = get_template_directory() . '/assets/icons/' . $family_dir . '/';
 
@@ -81,6 +105,6 @@ function fa( string $icon_name, string $family_dir = 'solid' ) {
 		return;
 	}
 
-	svg( $icon_name . '.svg', $full_family_dir );
+	return get_svg( $icon_name . '.svg', $full_family_dir );
 
 }
