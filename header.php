f8607bd090044c3f01201fc14327b1db1918023d
Michelle Blanchette <michelle@purpleturtlecreative.com>
2020-12-18 08:58:26 -0500

    ENH: preconnect to google fonts

--- header.php
+++ header.php
@@ -19,6 +19,7 @@ namespace PTC_Theme;
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="preconnect" href="https://fonts.gstatic.com">
   <link rel="preload" href="https://fonts.gstatic.com/s/poppins/v15/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2" as="font" type="font/woff2" crossorigin="anonymous">
   <link rel="preload" href="https://fonts.gstatic.com/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4AMP6lQ.woff2" as="font" type="font/woff2" crossorigin="anonymous">
 
