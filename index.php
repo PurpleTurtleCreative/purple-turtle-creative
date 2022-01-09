73389d2857f16cb859eb50a141e9e5c1cde13cb8
Michelle Blanchette <michelle@purpleturtlecreative.com>
2022-01-09 12:17:10 -0500

    #15 - portfolio post preview card styling

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ get_header();
 				while ( have_posts() ) :
 					the_post();
 
-					get_template_part( 'template-parts/content', 'post-preview' );
+					get_template_part( 'template-parts/content', get_post_type() . '-preview' );
 
 				endwhile;
 				?>
