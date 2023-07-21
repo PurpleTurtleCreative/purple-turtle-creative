5c68d0f6466396c33137e41c82fe3019ce1dd4c2
Michelle Blanchette <michelle@purpleturtlecreative.com>
2023-07-21 12:43:47 -0400

    untested database install for mailing list email verifications wip

--- classes/public/class-mailing-lists.php
+++ classes/public/class-mailing-lists.php
@@ -33,6 +33,31 @@ class Mailing_Lists {
 	private const REQUESTS_LIMIT_PERIOD = 14 * \DAY_IN_SECONDS;
 
 	/**
+	 * The latest database version used by this class.
+	 *
+	 * @var int DATABASE_VERSION
+	 */
+	private const DATABASE_VERSION = 1;
+
+	/**
+	 * The option name of the currently installed database version.
+	 *
+	 * @var string DATABASE_VERSION_OPTION
+	 */
+	private const DATABASE_VERSION_OPTION = 'ptc_mailing_lists_db_version';
+
+	/**
+	 * The database table name for storing email verification
+	 * requests for mailing list subscribers.
+	 *
+	 * Note that this table DOES NOT USE THE WPDB PREFIX. That is
+	 * because this is a domain-level (global) table.
+	 *
+	 * @var string DATABASE_EMAIL_VERIFICATION_TABLE
+	 */
+	private const DATABASE_EMAIL_VERIFICATION_TABLE = 'ptc_mailing_lists_email_verification';
+
+	/**
 	 * The list of valid mailing list IDs mapped to the actual
 	 * mailing list which they represent.
 	 *
@@ -49,16 +74,56 @@ class Mailing_Lists {
 	 */
 	private const MAILING_LIST_IDS = array(
 		'completionist@purpleturtlecreative.com' => 'RN3epj3Y8VmA9iKj',
-		'mail.test@purpleturtlecreative.com' => '4T3HmaE8JtUovVF7',
+		'mail.test@purpleturtlecreative.com'     => '4T3HmaE8JtUovVF7',
 	);
 
 	/**
 	 * Hooks code into WordPress.
 	 */
 	public static function register() {
-		add_action( 'wp_enqueue_scripts', __CLASS__ . '::register_scripts' );
+		add_action( 'after_setup_theme', __CLASS__ . '::maybe_install_database_tables' );
 		add_action( 'html_routes_init', __CLASS__ . '::register_html_routes' );
 		add_action( 'rest_api_init', __CLASS__ . '::register_rest_routes' );
+		add_action( 'wp_enqueue_scripts', __CLASS__ . '::register_scripts' );
+	}
+
+	/**
+	 * Installs the database tables if not already up-to-date.
+	 */
+	public static function maybe_install_database_tables() {
+
+		$installed_version = (int) get_option( static::DATABASE_VERSION_OPTION, 0 );
+
+		if ( static::DATABASE_VERSION === $installed_version ) {
+			// Already installed.
+			return;
+		}
+
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+		$email_verification_table = static::DATABASE_EMAIL_VERIFICATION_TABLE;
+		dbDelta(
+			"CREATE TABLE {$email_verification_table} (
+				ID bigint(20) unsigned NOT NULL AUTO_INCREMENT UNIQUE,
+				email varchar(100) NOT NULL,
+				mailing_list varchar(100) NOT NULL,
+				verification_token char(32) NOT NULL,
+				status varchar(20) NOT NULL,
+				request_count smallint unsigned NOT NULL,
+				first_seen datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+				last_seen datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+				PRIMARY KEY  (ID)
+			) {$charset_collate};"
+		);
+
+		update_option(
+			static::DATABASE_VERSION_OPTION,
+			static::DATABASE_VERSION,
+			true // autoload.
+		);
 	}
 
 	/**
@@ -83,7 +148,6 @@ class Mailing_Lists {
 		string $captcha_action,
 		string $submit_label = 'Subscribe'
 	) {
-		// @TODO - Build the REST API endpoint to process the submission.
 		// @TODO - Write the JavaScript for asynchronous submission.
 		// @TODO - Define the ACF block for custom render placement.
 
@@ -98,7 +162,7 @@ class Mailing_Lists {
 		$form_action_url = rest_url( REST_API_NAMESPACE_V1 . '/mailing-lists/subscribe' );
 
 		?>
-			<form method="POST" action="<?php echo esc_url( $form_action_url ); ?>">
+			<form class="ptc-mailing-list-subscribe" method="POST" action="<?php echo esc_url( $form_action_url ); ?>">
 				<?php
 				if ( ! empty( $title_text ) ) {
 					echo '<h3>' . esc_html( $title_text ) . '</h3>';
@@ -215,5 +279,55 @@ class Mailing_Lists {
 		);
 
 		return new \WP_REST_Response( $response, 200 );
+	}
+
+	private static function add_email_verification_request(
+		string $email,
+		string $mailing_list
+	) {
+		// @TODO - Check if already exists.
+		// @TODO - Generate verification token.
+		// @TODO - Add email to database as pending.
+	}
+
+	/**
+	 * Gets the email verification record.
+	 *
+	 * @param string $email The email.
+	 * @param string $mailing_list The mailing list.
+	 *
+	 * @return array|null An associative array or null.
+	 */
+	private static function get_email_verification_record(
+		string $email,
+		string $mailing_list
+	) : ?array {
+
+		global $wpdb;
+		$res = $wpdb->get_row(
+			$wpdb->prepare(
+				"
+				SELECT *
+				FROM %i
+				WHERE email = %s
+				  AND mailing_list = %s
+				",
+				static::DATABASE_EMAIL_VERIFICATION_TABLE,
+				$email,
+				$mailing_list
+			),
+			\ARRAY_A
+		);
+	}
+
+	private static function update_email_verification_record(
+		array $col_values
+	) {}
+
+	private static function send_email_verification_request(
+		string $email,
+		string $mailing_list
+	) {
+		// @TODO - Send Mailgun email template with verification link.
 	}
 }
