81d55f32ac37df2d5d086b2cbde2985c47e7f1a5
Michelle Blanchette <michelle@purpleturtlecreative.com>
2023-07-20 23:07:21 -0400

    fixed 404 HTML Routes to not query and display blog posts in 404 template, optimized code registration hooks, made hooked functions generically useful

--- classes/public/class-html-routes.php
+++ classes/public/class-html-routes.php
@@ -16,7 +16,7 @@ class HTML_Routes {
 	 *
 	 * @var string $current_route
 	 */
-	private static $current_route = '';
+	private static $current_route;
 
 	/**
 	 * The path prefix for HTML route paths.
@@ -126,7 +126,11 @@ class HTML_Routes {
 		) {
 			// The main query is for a potential HTML route.
 
-			if ( static::is_endpoint_query( $query ) ) {
+			// Posts are never needed for an HTML route,
+			// whether HTTP 404 or 200.
+			add_filter( 'posts_request', __CLASS__ . '::remove_posts_request', PHP_INT_MAX, 2 );
+
+			if ( static::is_valid_route( $query->query['html_route'] ) ) {
 
 				// The main query has a valid HTML route endpoint.
 				static::$current_route = $query->query['html_route'];
@@ -139,11 +143,10 @@ class HTML_Routes {
 				do_action( 'html_route', static::$current_route );
 
 				// Register modifications for HTML routes.
-				add_filter( 'posts_request', __CLASS__ . '::modify_posts_request', PHP_INT_MAX, 2 );
 				add_action( 'template_redirect', __CLASS__ . '::optimize_html_route', 10, 0 );
 				add_filter( 'wp_header', __CLASS__ . '::modify_http_headers', PHP_INT_MAX, 1 );
 				add_filter( 'body_class', __CLASS__ . '::get_body_classes', 9, 0 );
-				add_filter( 'template_include', __CLASS__ . '::get_endpoint_template', 9, 0 );
+				add_filter( 'template_include', __CLASS__ . '::get_current_route_template', 9, 0 );
 			} else {
 				// The HTML route doesn't have a valid endpoint.
 				$query->set_404();
@@ -174,25 +177,30 @@ class HTML_Routes {
 	}
 
 	/**
-	 * Alters the posts SQL statement for the given query.
+	 * Alters the posts SQL statement to return no results.
 	 *
 	 * @param string    $sql The SQL statement.
 	 * @param \WP_Query $query The query.
 	 *
-	 * @return string The SQL statement.
+	 * @return bool Always false.
 	 */
-	public static function modify_posts_request( $sql, $query ) {
+	public static function remove_posts_request( $sql, $query ) {
 		// Prevent querying posts unnecessarily for HTML endpoints.
 		$query->set( 'no_found_rows', true );
 		$query->set( 'cache_results', false );
 		return false;
 	}
 
+	/**
+	 * Optimizes the request for HTML route endpoints.
+	 */
 	public static function optimize_html_route() {
 
 		// Remove Yoast SEO.
-		$front_end = \YoastSEO()->classes->get( \Yoast\WP\SEO\Integrations\Front_End_Integration::class );
-		remove_action( 'wpseo_head', [ $front_end, 'present_head' ], -9999 );
+		if ( function_exists( '\YoastSEO' ) ) {
+			$front_end = \YoastSEO()->classes->get( \Yoast\WP\SEO\Integrations\Front_End_Integration::class );
+			remove_action( 'wpseo_head', array( $front_end, 'present_head' ), -9999 );
+		}
 
 		// Disable WP Rocket caching.
 		add_filter( 'do_rocket_generate_caching_files', '__return_false' );
@@ -206,62 +214,55 @@ class HTML_Routes {
 	}
 
 	/**
-	 * Gets the HTML route from the query.
+	 * Gets the body classes for all HTML route endpoint pages.
 	 *
-	 * @param \WP_Query|null $query Optional. The query. Default
-	 * null to use the global $wp_query.
-	 *
-	 * @return string The HTML route. Empty string if none.
+	 * @return string[] The HTML class strings.
 	 */
-	public static function get_queried_route( $query = null ) {
-
-		$html_route = '';
-
-		if ( null === $query ) {
-			// Use global wp_query.
-			$html_route = get_query_var( 'html_route', '' );
-		} else {
-			$html_route = $query->get( 'html_route', '' );
-		}
-
-		return $html_route;
-	}
-
-	/**
-	 * Checks if the query is for a valid HTML route.
-	 *
-	 * @param \WP_Query|null $query Optional. The query. Default
-	 * null to use the global $wp_query.
-	 *
-	 * @return bool
-	 */
-	public static function is_endpoint_query( $query = null ) {
-		return ( ! empty( static::get_endpoint_template( $query ) ) );
-	}
-
-	/**
-	 * Gets the located HTML route endpoint template file.
-	 *
-	 * @param \WP_Query|null $query Optional. The query. Default
-	 * null to use the global $wp_query.
-	 *
-	 * @return string The located template file. Empty string on
-	 * failure.
-	 */
-	public static function get_endpoint_template( $query = null ) {
-		$html_route = static::get_queried_route( $query );
-		return static::$endpoints[ $html_route ] ?? '';
-	}
-
-	/**
-	 * Alters the array of HTML classes for the <body> tag.
-	 *
-	 * @param array $classes The HTML classes.
-	 *
-	 * @return array
-	 */
-	public static function get_body_classes() {
-		// Clear existing classes and set content type main class.
+	public static function get_body_classes() : array {
 		return array( 'html-route-endpoint' );
+	}
+
+	/**
+	 * Checks if the given HTML route is valid.
+	 *
+	 * @param string $route_path The HTML route.
+	 *
+	 * @return bool True if valid.
+	 */
+	public static function is_valid_route( string $route_path ) : bool {
+		return ( ! empty( static::get_route_template( $route_path ) ) );
+	}
+
+	/**
+	 * Gets the registered template file for the given HTML route.
+	 *
+	 * @param string $route_path The HTML route.
+	 *
+	 * @return string The HTML route template file. Default ''.
+	 */
+	public static function get_route_template( string $route_path ) : string {
+		return static::$endpoints[ $route_path ] ?? '';
+	}
+
+	/**
+	 * Gets the current HTML route.
+	 *
+	 * @return string The current HTML route. Default ''.
+	 */
+	public static function get_current_route() {
+		return (
+			empty( static::$current_route ) ?
+			'' :
+			static::$current_route
+		);
+	}
+
+	/**
+	 * Gets the registered template file for the current HTML route.
+	 *
+	 * @return string The current HTML route template file. Default ''.
+	 */
+	public static function get_current_route_template() : string {
+		return static::get_route_template( static::get_current_route() );
 	}
 }
