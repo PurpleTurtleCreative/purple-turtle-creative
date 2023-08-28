5ee92557057bfb347fe318688b8068521c5595da
Michelle Blanchette <michelle@purpleturtlecreative.com>
2023-08-28 17:20:24 -0400

    move registered block styles to SCSS files that are automatically detected and loaded

--- functions/editor-style.php
+++ functions/editor-style.php
@@ -9,6 +9,7 @@ namespace PTC_Theme;
 
 defined( 'ABSPATH' ) || die();
 
+add_action( 'init', __NAMESPACE__ . '\register_block_customizations', 10 );
 add_action( 'after_setup_theme', __NAMESPACE__ . '\configure_gutenberg_support', 10 );
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets', 999 );
 add_filter( 'mkaz_code_syntax_language_list', __NAMESPACE__ . '\mkaz_code_syntax_language_list', 999, 1 );
@@ -16,132 +17,36 @@ add_filter( 'mkaz_code_syntax_language_list', __NAMESPACE__ . '\mkaz_code_syntax
 // Remove SVG definitions for duotones.
 remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
 
-register_block_style(
-	'core/columns',
-	[
-		'name'         => 'cards',
-		'label'        => 'Cards',
-		'inline_style' => '
-			.wp-block-columns.is-style-cards {
-				gap: 1em;
-				margin-bottom: 1em;
-			}
-			.wp-block-columns.is-style-cards .wp-block-column {
-				border: 2px solid #e4e6fd;
-				border-radius: .625rem;
-				padding: .625rem 1.625rem;
-			}
-	',
-	]
-);
+/**
+ * Registers custom block types and styles.
+ */
+function register_block_customizations() {
 
-register_block_style(
-	'core/group',
-	[
-		'name'         => 'fading-curve-top',
-		'label'        => 'Fading Curve Top',
-		'inline_style' => '
-			.wp-block-group.is-style-fading-curve-top {
-				background-image: linear-gradient(#f6f7fd, rgba(255,255,255,0));
-				border-radius: 50%/4%;
-			}
-			@media (min-width: 768px) {
-				.wp-block-group.is-style-fading-curve-top {
-					border-radius: 50%/15%;
-				}
-			}
-	',
-	]
-);
+	// Register block styles.
 
-register_block_style(
-	'core/list',
-	[
-		'name'         => 'clean',
-		'label'        => 'Clean',
-		'inline_style' => '
-			ul.is-style-clean {
-				margin: 0;
-				padding: 0 0 0 20px;
-				list-style-type: none;
-			}
-			p + ul.is-style-clean {
-				margin-top: -0.5em;
-			}
-			ul.is-style-clean li {
-				position: relative;
-				margin: 1em 0;
-			}
-			ul.is-style-clean li::before {
-				content: "";
-				display: block;
-				background: currentColor;
-				height: 6px;
-				width: 6px;
-				border-radius: 999px;
-				position: absolute;
-				left: -18px;
-				top: calc(0.5em + 3px);
-			}
-	',
-	]
-);
+	$block_style_headers = array(
+		'Block Type' => 'Block Type',
+		'Style Name' => 'Style Name',
+		'Style Label' => 'Style Label',
+	);
 
-register_block_style(
-	'core/gallery',
-	[
-		'name'  => 'popover-alt-text',
-		'label' => 'Popover Alt Text',
-		'inline_style' => '
-			.wp-block-gallery.is-style-popover-alt-text {
-				--wp--style--gallery-gap-default: 1rem;
-				padding: 0.5rem 2rem;
-			}
-			.wp-block-gallery.is-style-popover-alt-text figure.wp-block-image {
-				position: relative;
-			}
-			.wp-block-gallery.is-style-popover-alt-text figure.wp-block-image p.popover-alt-text {
-				display: none;
-			}
-			.wp-block-gallery.is-style-popover-alt-text figure.wp-block-image:hover p.popover-alt-text {
-				display: block;
-				position: absolute;
-				top: -10px;
-				left: 50%;
-				transform: translate(-50%,-100%);
-				text-align: center;
-				line-height: 1.3;
-				background: #08082b;
-				color: white;
-				padding: 7px 10px;
-				margin: 0;
-				border-radius: 5px;
-				user-select: none;
-				pointer-events: none;
-				z-index: 1;
-			}
-			@media (min-width: 768px) {
-				.wp-block-gallery.is-style-popover-alt-text figure.wp-block-image:hover p.popover-alt-text {
-					white-space: nowrap;
-					max-width: 100vw;
-				}
-			}
-			.wp-block-gallery.is-style-popover-alt-text figure.wp-block-image p.popover-alt-text::after {
-				content: "";
-				border-top: 7px solid #08082b;
-				border-left: 10px solid transparent;
-				border-right: 10px solid transparent;
-				border-bottom: 10px solid transparent;
-				position: absolute;
-				bottom: 1px;
-				transform: translate(-50%,100%);
-				left: 50%;
-				width: 0;
-				height: 0;
-			}
-		',
-	]
-);
+	// Find block styles.
+	foreach ( glob( THEME_PATH . '/assets/styles/block-style_*.css' ) as $stylesheet ) {
+		// Get block style file metadata.
+		$metadata = get_file_data( $stylesheet, $block_style_headers );
+		if ( ! empty( $metadata ) ) {
+			// Register the block style.
+			register_block_style(
+				$metadata['Block Type'],
+				array(
+					'name'         => $metadata['Style Name'],
+					'label'        => $metadata['Style Label'],
+					'inline_style' => file_get_contents( $stylesheet ),
+				)
+			);
+		}
+	}
+}
 
 /**
  * Configure the Gutenberg Editor.
